f14a610e9f87f423e763cdfd82a06e9ed9e70ce2
RonakAgarwal <agarwalronak02@gmail.com>
2019-12-20 15:50:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,16 +1,15 @@
-class Student 
+class Student
 
-  def hello 
+  def hello
 
     puts "Hey there! I'm so excited to learn stuff."
 
   end
 
-  def raise_hand 
+  def raise_hand
 
     puts "Pick me!"
 
   end
 
 end
-
